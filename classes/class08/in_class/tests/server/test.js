9741f245af7a98b2919dba15967f4a0ec86dd2f8
Josh Langowitz <jlangowitz@gmail.com>
2015-02-12 18:18:28 -0500

    barebones app with testing and task running

--- classes/class08/in_class/tests/server/test.js
+++ classes/class08/in_class/tests/server/test.js
@@ -7,9 +7,18 @@ var index = require('../../routes/index');
 
 // Sample tests
 describe("A test suite", function() {
+<<<<<<< HEAD
 	// Syncronous
 	it('should use expect syntax', function() {
 		expect(true).to.be.true|expect(true).to.be.false;
+=======
+	beforeEach(function() { });
+	afterEach(function() { });
+
+	// Syncronous
+	it('should pass', function() { 
+		expect(true).to.be.true; 
+>>>>>>> barebones app with testing and task running
 	});
 
 	// Async
@@ -25,4 +34,8 @@ describe("index", function() {
 	it('should have an attribute ten equal to 10', function() {
 		expect(index.ten).to.equal(10);
 	});
+<<<<<<< HEAD
 });
+=======
+})
+>>>>>>> barebones app with testing and task running
