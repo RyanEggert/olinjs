90022524c4221fbe37c559edd18cb097abe827df
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-02-17 17:03:09 -0500

    Fixed highlight tag. Adding OAuth

--- homework/cheepr/app.js
+++ homework/cheepr/app.js
@@ -15,6 +15,32 @@ var mongoose = require("mongoose");
 var cheeprs = require("./routes/cheepr");
 var users = require("./routes/users");
 var authrs = require("./routes/auth");
+var config = require('./oauth.js');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+
+// passport serialize and deserialize
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// passport config
+passport.use(new FacebookStrategy({
+    clientID: config.facebook.clientID,
+    clientSecret: config.facebook.clientSecret,
+    callbackURL: config.facebook.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+      return done(null, profile);
+    });
+  }
+));
+
 
 // app creation & configuration
 var app = express();
@@ -39,6 +65,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // routes
 app.get('/', cheeprs.home);
@@ -51,8 +80,34 @@ app.post('/users/new/', users.new);
 app.post('/cheep/new/', cheeprs.new);
 app.delete('/cheep/delete/', cheeprs.delete);
 
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res) {});
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    failureRedirect: '/'
+  }),
+  function(req, res) {
+    res.redirect('/');
+  });
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+
 // connections
 mongoose.connect(mongoURI);
 app.listen(PORT, function() {
   console.log("Application running on port:", PORT);
 });
+
+// other functions
+// test authentication
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
